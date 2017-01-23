79343f2e6c5747fcc3828c42e2e9e109f05ee581
GigaAxeL <gigaaxel@mail.ru>
2017-01-23 23:05:54 +0500

    День 3 конец... Заработала синхронная загрузка JSON Есть таймер

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 
 import {BranchesService} from "./app.service";
+import any = jasmine.any;
 
 
 @Component({
@@ -10,12 +11,12 @@ templateUrl: '/app/app.component.html'
 })
 
 export class AppComponent implements OnInit{
-  config = {};
+  config = {"refreshTime": 0};
   branches: any;
 
   constructor(public branchesService: BranchesService) {}
 
-  getConfigs():any {
+  getConfigs() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '../data/config.json', false);
     xhr.send();
@@ -39,13 +40,59 @@ export class AppComponent implements OnInit{
     }
   }
 
-  ngOnInit(): any {
+  // checkCams() {
+  //   var i;
+  //   for(i = 0; i < this.branches.length; i++) {
+  //     var b = this.branches[i];
+  //     this.isImage(b).then(function (test: any) {
+  //       console.log(test);
+  //     })
+  //   }
+  // }
+  //
+  // isImage(branch: any): Promise<any> {
+  //
+  // var image = new Image();
+  //  image.src = branch.imgpath;
+  //  return new Promise((resolve, reject) => {
+  //    if (image.onload) {
+  //      resolve(console.log("Cam " + branch + " connected..."));
+  //    } else {
+  //      reject(console.log("Cam " + branch + " no connect..."));
+  //    }
+  //  });
+
+
+
+  // image.onerror = function () {
+  //   branch.active = true;
+  //   //   branch.imgpath = "images/noconnect.jpg";
+  //   //    checkCams();
+  //   deferred.resolve(false);
+  // };
+  // image.onload = function () {
+  //   branch.active = true;
+  //   branch.updTime = new Date().getTime();
+  //   deferred.resolve(true);
+  //
+  // };
+  // image.src = branch.imgpath;
+  // // console.log("---1--->" + $scope.branches);
+  //
+  // return deferred.promise;
+  // }
+
+
+  ngOnInit() {
       this.getBranches();
       this.getConfigs();
       // console.log('--> ngOnInit() fired ' + typeof(this.config) + " = " + this.config.refreshTime);
 
-      // let timer = Observable.interval(15000);
-      // timer.subscribe();
+      let timer = Observable.timer(0, this.config.refreshTime);
+      timer.subscribe(t=> {
+        console.log("huesos");
+        this.getBranches();
+      });
   }
 
 
